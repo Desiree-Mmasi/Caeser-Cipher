73d30b227064d7f967cf2981f2c249e10edf84c5
Moringa School <mmasidesiree9@gmail.com>
2019-06-18 22:39:41 +0300

    This commit will add the backend for the encryption of the CaeserCipher project

--- src/main/java/CaeserCipher.java
+++ src/main/java/CaeserCipher.java
@@ -16,11 +16,40 @@ public class CaeserCipher {
     public int getKey() {
         return mKey;
     }
-
-    /*public String isEncrypted() {
+    public String isEncrypted() {
         mStatement = "b";
         return mStatement;
-    }*/
+    }
+
+    public String isActuallyEncrypted(String statement, int key) {
+
+        String result = "";
+        StringBuffer encrypted = new StringBuffer();
+
+
+        for (int i = 0; i < statement.length(); i++) {
+            int encrypt = statement.charAt(i);
+
+
+
+
+            if(encrypt + key > 122) {
+                char encrypts = (char) (encrypt - 26 +key);
+                encrypted.append(encrypts);
+            }
+
+
+            else {
+                char encrypts = (char) (encrypt + (key % 26));
+                encrypted.append(encrypts);
+
+
+
+            }
+        }
+        result += encrypted;
+        return result;
+    }
 
 
 }
