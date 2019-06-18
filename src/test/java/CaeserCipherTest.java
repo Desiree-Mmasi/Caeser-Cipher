8822870f58c5a9bcf0661628be02ec83eb017da0
Moringa School <mmasidesiree9@gmail.com>
2019-06-18 23:18:54 +0300

    This commit adds three more tests done

--- src/test/java/CaeserCipherTest.java
+++ src/test/java/CaeserCipherTest.java
@@ -8,4 +8,23 @@ public class CaeserCipherTest {
         CaeserCipher testCaesarCipher = new CaeserCipher("a", 2);
         assertEquals(true, testCaesarCipher instanceof CaeserCipher);
     }
+
+    @Test
+    public void newCaesarCipher_readsAString_a() {
+        CaeserCipher testCaesarCipher = new CaeserCipher("a", 2);
+        assertEquals("a", testCaesarCipher.getStatement());
+    }
+
+    @Test
+    public void newCaesarCipher_readKey_2() {
+        CaeserCipher testCaesarCipher = new CaeserCipher("a", 2);
+        assertEquals(2, testCaesarCipher.getKey());
+    }
+
+    @Test
+    public void newCaesarCipher_replaceLetter_b() {
+        CaeserCipher testCaesarCipher = new CaeserCipher("a", 2);
+        assertEquals("b", testCaesarCipher.isEncrypted());
+
+    }
 }
