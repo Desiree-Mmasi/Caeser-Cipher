abc5fbab6a6a4e07de6f1ef5aa2ecf3be201b3fa
Moringa School <mmasidesiree9@gmail.com>
2019-06-18 22:55:59 +0300

    This commit will add the Frontend to our code of Encryption

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,23 @@ public class App {
     public static void main(String[] args) {
         Console myConsole = System.console();
         Scanner myScanner = new Scanner(System.in);
+        boolean runProgram = true;
+
+        while (runProgram) {
+            System.out.println("Welcome to a Caesar Ciphar program. Would you like to encrypt or decrypt a statement or would you like to exit?");
+            String userAnswer = myConsole.readLine();
+
+
+            if (userAnswer.equals("encrypt")) {
+                System.out.println("Please enter a statement that you would like to encrypt:");
+                String statement = myConsole.readLine();
+
+                System.out.println("Please enter an encryption key");
+                int key = myScanner.nextInt();
+
+                CaeserCipher userEncryption = new CaeserCipher(statement, key);
+                String encryption = userEncryption.isActuallyEncrypted(statement, key);
+
+                System.out.println("The encrypted statement is: " + encryption);
     };
 }
