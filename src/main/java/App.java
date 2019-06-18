eb357397b382eb75ae090fb3f07c2f72e6499bbe
Moringa School <mmasidesiree9@gmail.com>
2019-06-18 22:59:35 +0300

    This commit adds the Frontend for our decryption code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,5 +23,22 @@ public class App {
                 String encryption = userEncryption.isActuallyEncrypted(statement, key);
 
                 System.out.println("The encrypted statement is: " + encryption);
-    };
+            } else if (userAnswer.equals("decrypt")) {
+                System.out.println("Please enter a statement to decrypt: ");
+                String statementDecrypt = myConsole.readLine();
+
+                System.out.println("Please enter an decryption key");
+                int keyDecrypt = myScanner.nextInt();
+
+                CaeserCipher userDecryption = new CaeserCipher(statementDecrypt, keyDecrypt);
+                String decryption = userDecryption.toDecrypt(statementDecrypt, keyDecrypt);
+
+                System.out.println("The decrypted statement is: " + decryption);
+            } else if (userAnswer.equals("exit")) {
+                runProgram = false;
+            } else {
+                System.out.println("Sorry we could not recognise your input");
+            }
+        }
+    }
 }
