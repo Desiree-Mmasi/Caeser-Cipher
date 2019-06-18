2c6af2335dd4866c16eb6b784fb60e4d26eda139
Moringa School <mmasidesiree9@gmail.com>
2019-06-18 22:48:01 +0300

    This commit will add the initial interaction of our application with our user(s) so that I gather information required for the application to run

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Console myConsole = System.console();
+        Scanner myScanner = new Scanner(System.in);
+    };
+}
